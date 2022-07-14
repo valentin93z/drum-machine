bf6b84124d972b7e65b722be101d27a44f8c35c5
valentin93z <valentin.zagrebun.93@gmail.com>
2022-07-14 18:27:33 +0300

    add working display, second sound group

--- src/App.js
+++ src/App.js
@@ -57,40 +57,113 @@ const firstSoundGroup = [
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }
   ];
 
+  const secondSoundGroup = [
+    {
+      keyCode: 81,
+      key: 'Q',
+      id: 'Chord-1',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3' },
+    
+    {
+      keyCode: 87,
+      key: 'W',
+      id: 'Chord-2',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3' },
+    
+    {
+      keyCode: 69,
+      key: 'E',
+      id: 'Chord-3',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3' },
+    
+    {
+      keyCode: 65,
+      key: 'A',
+      id: 'Shaker',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3' },
+    
+    {
+      keyCode: 83,
+      key: 'S',
+      id: 'Open-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3' },
+    
+    {
+      keyCode: 68,
+      key: 'D',
+      id: 'Closed-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3' },
+    
+    {
+      keyCode: 90,
+      key: 'Z',
+      id: 'Punchy-Kick',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3' },
+    
+    {
+      keyCode: 88,
+      key: 'X',
+      id: 'Side-Stick',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3' },
+    
+    {
+      keyCode: 67,
+      key: 'C',
+      id: 'Snare',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3' }
+  ];
+
 
 function App() {
 
   const [volume, setVolume] = useState(0.5);
+  const [group, setGroup] = useState("control-bank-first-group");
+  const [viewDisplay, setViewDisplay] = useState("");
+
+  const groupSwitcher = () => {
+    if (group === "control-bank-first-group") {
+      setGroup("control-bank-second-group");
+      setViewDisplay("Second Sound Group");
+    } else {
+      setGroup("control-bank-first-group");
+      setViewDisplay("First Sound Group");
+    }
+  }
+
+  const currentGroup = group === "control-bank-first-group" ? firstSoundGroup : secondSoundGroup;
 
   return (
     <div className="App" id='drum-machine'>
         <div className='drum-keyboard'>
-          {firstSoundGroup.map((sound) => (
-          <Pad key={sound.id} sound={sound} volume={volume} />))}
+          {currentGroup.map((sound) => (
+          <Pad key={sound.id} sound={sound} volume={volume} setViewDisplay={setViewDisplay} />))}
         </div>
         <div className='drum-control'>
           <div className='control-power'>
-            <p>power</p>
+            <p>Power</p>
             <div className='switcher'>
               <div className='switch'></div>
             </div>
           </div>
           <div className='display' id='display'>
-            text on dispaly
+            {viewDisplay}
           </div>
           <div className='control-volume'>
             <input 
               type="range"
-              onChange={(e) => setVolume(e.target.value)}
+              onChange={(e) => {
+                setVolume(e.target.value);
+                setViewDisplay("Volume: " + e.target.value * 100 + "%");
+              }}
               value={volume}
               step="0.01"
               min="0"
               max="1" />
           </div>
           <div className='control-bank'>
-            <p>bank</p>
+            <p>Sound Group</p>
             <div className='switcher'>
-              <div className='switch'></div>
+              <div className={`switch ${group}`} onClick={groupSwitcher}></div>
             </div>
           </div>
         </div>
@@ -100,7 +173,7 @@ function App() {
 
 
 
-function Pad({ sound, volume }) {
+function Pad({ sound, volume, setViewDisplay }) {
 
   const [active, setActive] = useState(false);
 
@@ -121,6 +194,7 @@ function Pad({ sound, volume }) {
     const audio = document.getElementById(sound.key);
     setActive(true);
     setTimeout(() => setActive(false), 200);
+    setViewDisplay(sound.id);
     audio.volume = volume;
     audio.currentTime = 0;
     audio.play();
